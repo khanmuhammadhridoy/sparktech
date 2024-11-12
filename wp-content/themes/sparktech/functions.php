98faecb491fb48cb090c5aa38f37c5e4f54cfe9e
khanmuhammadhridoy <khanmuhammadhridoy@gmail.com>
2024-11-12 17:13:48 +0600

    custom css in functin

--- wp-content/themes/sparktech/functions.php
+++ wp-content/themes/sparktech/functions.php
@@ -5,7 +5,7 @@ function sparktech_enqueue_styles()
     // Enqueue the main stylesheet (style.css in the theme root folder)
     wp_enqueue_style('sparktech-style', get_stylesheet_uri());
 
-    // Enqueue a custom stylesheet located in /css/custom-style.css
-    wp_enqueue_style('sparktech-custom-style', get_template_directory_uri() . '/css/custom-style.css');
+    // Enqueue a custom stylesheet
+    wp_enqueue_style('sparktech-custom-style', get_template_directory_uri() . '/assets/css/style.css');
 }
 add_action('wp_enqueue_scripts', 'sparktech_enqueue_styles');
