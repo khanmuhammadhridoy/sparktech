f5176e6a83ac803464b677dc029a3ac5999e9176
khanmuhammadhridoy <khanmuhammadhridoy@gmail.com>
2024-11-14 14:13:17 +0600

    hero

---
+++ wp-content/themes/sparktech/hero.php
@@ -0,0 +1,44 @@
+<?php ?>
+<!-- hero area starts -->
+<section class="hero-area">
+    <div class="hero-bg dark-version" style="background-image: url(assets/images/hero-bg.png)"></div>
+    <div class="hero-bg light-version" style="background-image: url(assets/images/hero-light-bg.png)"></div>
+    <div class="container">
+        <div class="hero-content-box">
+            <div class="row hero-top-content">
+                <div class="col-lg-7 offset-lg-1">
+                    <h1 class="mb-20 title_one">Crafting <span class="gradient-bg">Creativity</span></h1>
+                </div>
+                <div class="col-lg-4">
+                    <img src="assets/images/hero-3d-02.png" class="vector-shape vector-anim-1 item-moveOne"
+                        alt="3d-image">
+                </div>
+            </div>
+            <div class="row hero-bottom-content">
+                <div class="col-lg-5">
+                    <img src="assets/images/hero-3d-01.png" class="vector-shape vector-anim-2 item-moveTwo"
+                        alt="3d-image">
+                </div>
+                <div class="col-lg-7">
+                    <h1 class="title_one">Amplifying Impact!</h1>
+                    <p class="mt-20">We are a passionate collective of creatives, designers, and strategists dedicated
+                        to shaping remarkable brand experiences.</p>
+                    <div class="btn-group">
+                        <img class="dark-version" src="assets/images/vector-arrow.png" alt="vector-arrow">
+                        <img class="light-version" src="assets/images/vector-dark-arrow.png" alt="vector-arrow">
+                        <a href="about.html" class="secondary-btn">Learn More</a>
+                    </div>
+                </div>
+            </div>
+            <ul class="hero-social">
+                <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-x-twitter"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-linkedin"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+            </ul>
+            <a href="#counter-area"><img src="assets/images/vector-scroll-down.png" alt="vector-scroll-down"
+                    class="vector-scroll-down scroll-down"></a>
+        </div>
+    </div>
+</section>
